12811868e3965721ff76fab57ed507b644c3791b
Timur Ramazanov <timurram007@gmail.com>
2024-09-05 13:14:03 +0200

    Initial Commit

---
+++ BinaryGame/BinaryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  BinaryGameApp.swift
+//  BinaryGame
+//
+//  Created by Timur Ramazanov on 05.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct BinaryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
